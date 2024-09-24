b57c72eed0663f11f0e635774410a7a5a277fc9b
tantohtelmah <tantohtelmah@gmail.com>
2024-09-24 19:15:13 -0400

    issue - InputMismatchException

--- Lab_1/lab1_exer.java
+++ Lab_1/lab1_exer.java
@@ -1,4 +1,5 @@
 package Lab_1;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class lab1_exer {
@@ -10,82 +11,107 @@ public class lab1_exer {
 	
 		// Method
 		public String createPost() {
-			return author + " " + message;
+			return "\"" + author + " says " + message + "\"";
 		}
 	}
 
 	// Discussion Board object
 	public class DiscussionBoard {
 
-		//Attributes
-		int i, y = 0;
-		String positiveAns[] = {"yes", "yeah", "yup"};
+		// Class Attributes
 		String[] postStrings = new String[10]; //Arrays of posts
+		private int postCount = 0;
+		String positiveAns[] = {"yes", "yeah", "yup"};
 		Scanner scanner = new Scanner(System.in);
 		boolean stillPosting = false;
+		Post newPost = new Post();
 
 		//Methods
 		//Post new messages
 		public void postNewMessage(){
-			y = 0;
 			while (true){
-				Post newPost = new Post();
+				//Checking if the Discussion Board is full
+				if (postCount >= postStrings.length) {
+					System.out.println("Discussion Board is full. Sorry!");
+					break;
+				}
 				System.out.println("Please enter your name:");
 				newPost.author = scanner.nextLine();
 				System.out.println("Please write your thoughts");
 				newPost.message = scanner.nextLine();
-				System.out.println("\"" + newPost.author + " says: " + newPost.message + "\"");
-				System.out.println("Do yo still want to post another message?");
-				try {
-					postStrings[y] = newPost.createPost();
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.out.println("Discussion Board is full. Sorry!" + e.getMessage());
-					break;
-				}
+				// System.out.println("\"" + newPost.author + " says: " + newPost.message + "\"");
+
+				// Adding post to array(postStrings)
+				postStrings[postCount] = newPost.createPost();
+				postCount++;
+				System.out.println("Your message has been posted.");
 				
-				String stillPostingAns = scanner.nextLine();
-				for (i = 0; i < positiveAns.length; i++) {
-					if (stillPostingAns.toLowerCase().equals(positiveAns[i])) {
-						stillPosting = true;
-						break;
-					}
-				}
+				// Another option to post
+				System.out.println("Do you still want to post another message?");
+				String stillPostingAns = scanner.nextLine().toLowerCase();
+                            for (String positiveAn : positiveAns) {
+                                if (stillPostingAns.equals(positiveAn)) {
+                                    stillPosting = true;
+                                    break;
+                                }
+								else if(!stillPostingAns.equals("no")){
+									System.out.println("Invalid response, please use \"yes\" or \"no\", Thanks");
+								}
+                            }
 				if (!stillPosting) {
 					break;
 				}
 				stillPosting = false;
 			}
 		}
+
+		
+		
+		
 		//Prints all post
 		public void printAllPosts(){
-			for (i = 0; i < postStrings.length; i++){
-				System.out.println(postStrings[i]);
+			System.out.println("Printing all posts...");
+			try{
+				Thread.sleep(500);
+				for (int i = 0; i < postCount; i++){
+					if (postStrings[i] != null) {
+						System.out.println(postStrings[i]);
+					}
+				}
+			}catch(InterruptedException e) {
+				System.err.println("Thread was interrupted!");
 			}
 		}
 		//prints all post in reverse order
 		public void printReverse(){
-			//code
+			System.out.println("Printing all posts in reverse order...");
+			for (int i = postCount - 1; i >= 0; i--) {
+				if (postStrings[i] != null) {
+					System.out.println(postStrings[i]);
+				}
+			}
 		}
 		// print number of post enetered so far
-		public void printNUmOfPost(){
-			//code
+		public void printNumOfPosts(){
+        	System.out.println("Number of posts entered so far: " + postCount);
 		}
 		//print all post from a user
 		public void printPostOfUser(){
-			for (i = 0; postStrings.length < i; i++){
-				if (postStrings[i] == null){
-					System.out.println("Sorry Discussion Board is empty");
-					break;
-				}
-			}
 			System.out.println("Please enter name of user to view post");
 			String postUserName = scanner.nextLine().toLowerCase();
+			if (postCount == 0){
+				System.out.println("Sorry the Discussion Board is empty");
+			}
 			boolean found = false;
-			for (i = 0; i < postStrings.length; i++) {
-				if (postStrings[i] != null && postStrings[i].startsWith(postUserName)) {
-					System.out.println(i);
-					System.out.println(postStrings[i]);
+			for (int i = 0; i < postCount; i++) {
+				System.out.println("Hi");
+				if (postStrings[i].toLowerCase() != null && postStrings[i].toLowerCase().startsWith("\"" + postUserName)) {
+					System.out.println("Found post of " + postUserName + " at " + i + ".");
+					System.out.println("Post: " + postStrings[i]);
 					found = true;
+				}
+				else{
+					System.out.println("rather here" + postStrings[i]);
 				}
 			}
 			if (!found){
@@ -95,79 +121,105 @@ public class lab1_exer {
 
 		//Print number of vowels
 		public void printNumOfVowels(){
-			//code
+			int vowelCount = 0;
+			for (int i = 0; i < postCount; i++) {
+				String post = postStrings[i].toLowerCase();
+				for (char c : post.toCharArray()) {
+					if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+						vowelCount++;
+					}
+				}
+			}
+			System.out.println("Total number of vowels across all posts: " + vowelCount);
+		}
+
+
+		//Search case sensitive
+		public void caseSensitiveSearch(String searchWord){
+			boolean found = false;
+			for (int i = 0; i < postCount; i++) {
+				if (postStrings[i].contains(searchWord)) {
+					System.out.println("Found in post " + (i + 1) + ": " + postStrings[i]);
+					found = true;
+				}
+			}
+			if (!found) {
+				System.out.println("No posts contain the word \"" + searchWord + "\" (case-sensitive).");
+			}
 		}
 
 		//Search case sensitive
-		public void caseSensitiveSearch(){
-			//code
+		public void caseInSensitiveSearch(String searchWord){
+			boolean found = false;
+			for (int i = 0; i < postCount; i++) {
+				if (postStrings[i].toLowerCase().contains(searchWord.toLowerCase())) {
+					System.out.println("Found in post " + (i + 1) + ": " + postStrings[i]);
+					found = true;
+				}
+			}
+			if (!found) {
+				System.out.println("No posts contain the word \"" + searchWord + "\" (case-sensitive).");
+        }
 		}
 
-		//Search case sensitive
-		public void caseInSensitiveSearch(){
-			//code
-		}
-	}
-
-	public class Menu {
-		DiscussionBoard discussionBoard = new DiscussionBoard();
-		String post = "post";
-		String[] prints = {"All posts", "All posts in reverse order", "Number of posts enetered", "All posts from a specific user", "Print the number of vowels across all posts"};
-
-		public void printPost(String menuOption){
-			if(menuOption.equals(prints[0])){
-				discussionBoard.printAllPosts();
-			}
-			else if(menuOption.equals(prints[1])){
-				discussionBoard.printReverse();
-			}
-			else if(menuOption.equals(prints[2])){
-				discussionBoard.printNUmOfPost();
-			}
-			else if(menuOption.equals(prints[3])){
-				discussionBoard.printPostOfUser();
-			}
-			else if(menuOption.equals(prints[4])){
-				discussionBoard.printNumOfVowels();
-			}
+		//Exit Discussion Board
+		public void exitDiscussionBoard(){
+			System.exit(0);
 		}
 	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		lab1_exer outer = new lab1_exer();
-    	Menu menu = outer.new Menu();
+    	// Menu menu = outer.new Menu();
 		DiscussionBoard discussionBoard = outer.new DiscussionBoard();
 
-		while (true) { 
-			System.out.println("Do you wish to post, view a post or search through the posts with a given word?");
-			String optionToPerform = scanner.nextLine().toLowerCase();
-			if (optionToPerform.equals(menu.post)){
-				discussionBoard.postNewMessage();
-			}
-			else if(optionToPerform.equals("view post")){
-				System.out.println("Select which format you would like to see the post displayed...");
-				String optionToPerformPrint = scanner.nextLine().toLowerCase();
-				for (int i = 0; menu.prints.length < i; i++){
-					if (optionToPerformPrint.equals(menu.prints[i])){
-						menu.printPost(optionToPerform);
+		while (true) {
+            System.out.println("Choose an option:");
+            System.out.println("(1) Post new message");
+            System.out.println("(2) Print all posts");
+            System.out.println("(3) Print all posts in reverse order");
+            System.out.println("(4) Print number of posts entered so far");
+            System.out.println("(5) Print all posts from a user");
+            System.out.println("(6) Print the number of vowels across all posts");
+            System.out.println("(7) Perform a search of posts containing a given word (case sensitive)");
+            System.out.println("(8) Perform a search of posts containing a given word (case insensitive)");
+            System.out.println("(9) End Program");
+            int option = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (option) {
+                case 1 -> discussionBoard.postNewMessage();
+                case 2 -> discussionBoard.printAllPosts();
+                case 3 -> discussionBoard.printReverse();
+                case 4 -> discussionBoard.printNumOfPosts();
+                case 5 -> discussionBoard.printPostOfUser();
+                case 6 -> discussionBoard.printNumOfVowels();
+                case 7 -> {
+                    System.out.println("Enter word: ");
+                    String searchWord = scanner.nextLine();
+                    discussionBoard.caseSensitiveSearch(searchWord);
+                        }
+                case 8 -> {
+                    System.out.println("Enter word: ");
+                            String searchWord = scanner.nextLine();
+                            discussionBoard.caseInSensitiveSearch(searchWord);
+                        }
+                case 9 -> {
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                        }
+                default -> {
+					System.out.println("Invalid option. Please try again.");
+					try {
+						System.out.println("Invalid option. Please try again.");
+					} catch (InputMismatchException e) {
+						System.out.println("Error");
 					}
 				}
-			}else if(optionToPerform.contains("search")){
-				System.out.println("Would you like a case sensitive search or a case insentive search");
-				try {
-					String caseSearchOption = scanner.nextLine().toLowerCase();
-					if (caseSearchOption.contains("sensitive")){
-						discussionBoard.caseSensitiveSearch();
-					}
-					else if(caseSearchOption.contains("insensitive")){
-						discussionBoard.caseInSensitiveSearch();
-					}
-				} finally {
-					scanner.close();
-				}
-			}
-		}
+            }
+        }
 	}
 }
 
